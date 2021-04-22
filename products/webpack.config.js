ce608a7f5fa8e6fcde1667fcdf301b452d84424e
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-04-23 00:53:17 +0900

    make faker list page

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,3 +1,15 @@
+const path = require("path");
+const HTMLWebpackPlugin =  require("html-webpack-plugin");
+
 module.exports = {
-    mode:'development'
+    mode:'development',
+    plugins:[
+        new HTMLWebpackPlugin({
+            template:path.resolve(__dirname,"public","index.html")
+        }),
+    ],
+    devServer:{
+        port:8081
+    }
 }
+
