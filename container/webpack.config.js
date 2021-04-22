c0cc419bc8e011bd110c8527e02d373adddc5f43
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-04-23 01:15:45 +0900

    module federation plugin

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const HTMLWebpackPlugin =  require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
 
 module.exports = {
     mode:'development',
     plugins:[
+       new ModuleFederationPlugin({
+           name:'container',
+           remotes:{
+               products:'products@http://localhost:8081/remoteEntry.js'
+           }
+       }),
         new HTMLWebpackPlugin({
             template:path.resolve(__dirname,"public","index.html")
         }),
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const HTMLWebpackPlugin =  require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
 
 module.exports = {
     mode:'development',
     plugins:[
+        new ModuleFederationPlugin({
+            name:'products',
+            filename:'remoteEntry.js',
+            exposes:{
+                './ProductsIndex':'./src/index.js'
+            }
+        }),
         new HTMLWebpackPlugin({
             template:path.resolve(__dirname,"public","index.html")
         }),
