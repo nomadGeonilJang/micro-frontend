9229711bffbe10751b61c1bc7e979a1575d4b202
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-04-23 01:01:45 +0900

    make container and setup webapck

---
+++ container/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HTMLWebpackPlugin =  require("html-webpack-plugin");
+
+module.exports = {
+    mode:'development',
+    plugins:[
+        new HTMLWebpackPlugin({
+            template:path.resolve(__dirname,"public","index.html")
+        }),
+    ],
+    devServer:{
+        port:8080
+    }
+}
+
